90534f53414c80bc9252df3bf1e050456a0b13d4
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-04-22 09:35:49 +0530

    Update packerWorkflow.groovy

--- vars/packerWorkflow.groovy
+++ vars/packerWorkflow.groovy
@@ -24,3 +24,56 @@ def buildPacker(Map stepParams)
         )
     }
 }
+
+def call(Map stepParams) {
+    
+    try 
+    {
+        git.checkoutCode()
+    } 
+    catch (Exception e) 
+    {
+        echo "Unable to clone CodeBase"
+        echo e.toString()
+        throw e
+    }
+
+    try 
+    {
+        config = commonfile.readPropertyFile(
+            configFilePath: "${stepParams.configFilePath}"
+        )
+    }
+    catch (Exception e) 
+    {
+        echo "Sorry I'm unable to read Config file"
+        echo e.toString()
+        throw e
+    }
+
+    try 
+    {
+        validatePacker(
+            codeBasePath: "${config.CODE_BASE_PATH}",
+        )
+    } 
+    catch (Exception e) 
+    {
+        echo "Unable to Validate Packer Scripts"
+        echo e.toString()
+        throw e
+    }
+
+    try 
+    {
+        buildPacker(
+            codeBasePath: "${config.CODE_BASE_PATH}",
+        )
+    } 
+    catch (Exception e) 
+    {
+        echo "Unable to Build Packer"
+        echo e.toString()
+        throw e
+    }
+}
