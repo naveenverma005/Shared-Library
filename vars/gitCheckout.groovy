920060aef9ed4aef7e5df2e75f8451e2aa47954c
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-04-22 09:35:18 +0530

    Create gitCheckout.groovy

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+}
