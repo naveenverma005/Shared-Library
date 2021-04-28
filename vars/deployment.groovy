828587aad71847663d1d148a103d8aeff883036f
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-04-29 00:51:27 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -8,6 +8,7 @@ def deployment(Map stepParams)
             codePath: "${config.CODE_BASE_PATH}"
             sh 'aws ssm get-parameters-by-path --path /coinsfast/dev --recursive --with-decryption --output text   --query "Parameters[].[Name,Value]" | sed  -e \'s/\\/coinsfast\\///\' | sed -e \'s/dev\\///\' | sed -e \'s/$/"/g\' | sed -r \'s/\\s+/="/g\' > .env'
             sh 'ssh -i ${stepsParams.credentials} ${stepsParams.user}@${stepsParams.ipaddress_1} \'rm ${stepsParams.applicationPath}.env\''
+            sh 'echo "passed"'
             sh 'scp -i ${stepsParams.credentials} .env ${stepsParams.user}@${stepsParams.ipaddress_1}:${stepsParams.applicationPath}'
             sh 'ssh -i ${stepsParams.credentials} ${stepsParams.user}@${stepsParams.ipaddress_1} \'cp -prf ${stepsParams.applicationPath}* ${stepsParams.applicationBackup}\''
             sh 'ssh -i ${stepsParams.credentials} ${stepsParams.user}@${stepsParams.ipaddress_2} \'rm ${stepsParams.applicationPath}.env\''
