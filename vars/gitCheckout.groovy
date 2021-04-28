2f2d2968061fe4e449adb7e49a35b9d593ef1f94
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-04-28 23:17:16 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,8 +3,6 @@
 def checkoutCode() {
     stage("Checking out code repository") 
     {
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        checkout scm
     }
 }
