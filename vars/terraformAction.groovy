71d20245f372bfad5f3426f26bac84656cf37880
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-13 21:44:01 +0530

    Create terraformAction.groovy

---
+++ vars/terraformAction.groovy
@@ -0,0 +1,20 @@
+  
+#!/usr/bin env groovy
+def executeAction(Map stepParams) 
+{
+  dir("${stepParams.codePath}") 
+  {  
+    if ("${stepParams.operation}" == "apply")
+    {
+      sh "terraform ${stepParams.operation} -lock=false -auto-approve"
+    }
+    else
+    {
+      sh "terraform ${stepParams.operation}"
+    }
+    if ("${stepParams.operation}" == "destroy")
+    {
+      sh "terraform ${stepParams.operation} -lock=false -auto-approve"
+    }
+  }
+}
