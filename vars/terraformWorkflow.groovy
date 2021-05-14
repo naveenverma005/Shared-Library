1ab9cde56c3481c4749df7a330e218db89e9255f
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-14 20:12:22 +0530

    Update terraformWorkflow.groovy

--- vars/terraformWorkflow.groovy
+++ vars/terraformWorkflow.groovy
@@ -1,158 +1,199 @@
 #!/usr/bin/env groovy
 def initializeTerraform(Map stepParams) 
 {
-    stage("Terraform Initializing") 
-    {
-        terraformAction.executeAction(
-            codePath: "${config.CODE_BASE_PATH}",
-            operation: "init"
-        )
-    }
+  stage("Terraform Initializing") 
+  {
+    terraformAction.executeAction(
+      codePath: "${config.CODE_BASE_PATH}",
+      operation: "init"
+    )
+  }
 }
 def formattingTerraformCode(Map stepParams) 
 {
-    stage("Formatting Terraform Code") 
-    {
-        terraformAction.executeAction(
-            codePath: "${config.CODE_BASE_PATH}",
-            operation: "fmt -list=true -write=false -diff=true"
-        )
-    }
+  stage("Formatting Terraform Code") 
+  {
+    terraformAction.executeAction(
+      codePath: "${config.CODE_BASE_PATH}",
+      operation: "fmt -list=true -write=false -diff=true"
+    )
+  }
 }
 def validateTerraformCode(Map stepParams) 
 {
-    stage("Validating Terraform Code") 
-    {
-        terraformAction.executeAction(
-            codePath: "${config.CODE_BASE_PATH}",
-            operation: "validate"
-        )
-    }
+  stage("Validating Terraform Code") 
+  {
+    terraformAction.executeAction(
+      codePath: "${config.CODE_BASE_PATH}",
+      operation: "validate"
+    )
+  }
 }
 def planInfrastructure(Map stepParams) 
 {
-    stage("Planning Terraform Code") 
-    {
-        terraformAction.executeAction(
-            codePath: "${config.CODE_BASE_PATH}",
-            operation: "plan"
-        )
-    }
+  stage("Planning Terraform Code") 
+  {
+    terraformAction.executeAction(
+      codePath: "${config.CODE_BASE_PATH}",
+      operation: "plan"
+    )
+  }
 }
 def lintingTerraformCode(Map stepParams) 
 {
-    stage("Linting Terraform Code") 
-    {
-        terraformAction.executeLinting(
-            codePath: "${config.CODE_BASE_PATH}"
-        )
-    }
+  stage("Linting Terraform Code") 
+  {
+    terraformAction.executeLinting(
+      codePath: "${config.CODE_BASE_PATH}"
+    )
+  }
 }
 def createInfrastructure(Map stepParams) 
 {
-    stage("Applying Terraform") 
-    {
-        terraformAction.executeAction(
-            codePath: "${config.CODE_BASE_PATH}",
-            operation: "apply -lock=false -auto-approve"
-        )
-    }
+  stage("Applying Terraform") 
+  {
+    terraformAction.executeAction(
+      codePath: "${config.CODE_BASE_PATH}",
+      operation: "apply -lock=false -auto-approve"
+    )
+  }
 }
-
+def sendSuccessNotification(Map stepParams) 
+{
+  stage("Sending success notification on Google Chat") 
+  {
+    googleChatNotification.sendGoogleNotification(
+      buildStatus: "BUILD SUCCESSFUL",
+      message: "${stepParams.message}"
+    )
+  }
+}
+def sendFailNotification(Map stepParams) 
+{
+  stage("Sending failure notification on Google Chat") 
+  {
+    googleChatNotification.sendGoogleNotification(
+      buildStatus: "BUILD FAILED",
+      message: "${stepParams.message}"
+    )
+  }
+}
 def call(Map stepParams) {
-    try 
-    {
-        gitCheckout.checkoutCode()
-    } 
-    catch (Exception e) 
-    {
-        echo "Unable to clone CodeBase"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        config = commonfile.readPropertyFile(
-            configFilePath: "${stepParams.configFilePath}"
-        )
-    }
-    catch (Exception e) 
-    {
-        echo "Sorry I'm unable to read Config file"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        initializeTerraform(
-            codeBasePath: "${config.CODE_BASE_PATH}",
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Unable to initialize Terraform"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        formattingTerraformCode(
-            codeBasePath: "${config.CODE_BASE_PATH}"
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Failed while formatting Terraform Code! Please look into your code"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        validateTerraformCode(
-            codeBasePath: "${config.CODE_BASE_PATH}"
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Failed while Terraform Code Validation! Please look into your code"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        lintingTerraformCode(
-            codeBasePath: "${config.CODE_BASE_PATH}"
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Failed while linting Terraform Code! Please look into your code"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        planInfrastructure(
-            codeBasePath: "${config.CODE_BASE_PATH}",
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Failed during planning Infrastructure"
-        echo e.toString()
-        throw e
-    }
-    try 
-    {
-        input message: 'Press Yes to apply changes', ok: 'YES'
-        createInfrastructure(
-            codeBasePath: "${config.CODE_BASE_PATH}"
-        )
-    } 
-    catch (Exception e) 
-    {
-        echo "Unable to Apply Terraform"
-        echo e.toString()
-        throw e
-    }
+  try 
+  {
+    git.checkoutCode()
+  } 
+  catch (Exception e) 
+  {
+    echo "Unable to clone CodeBase"
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    config = commonfile.readPropertyFile(
+      configFilePath: "${stepParams.configFilePath}"
+    )
+  }
+  catch (Exception e) 
+  {
+    echo "Sorry I'm unable to read Config file"
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    initializeTerraform(
+      codeBasePath: "${config.CODE_BASE_PATH}",
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Unable to initialize Terraform"
+    sendFailNotification(
+      message: "Unable to initialize Terraform"
+    )
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    formattingTerraformCode(
+      codeBasePath: "${config.CODE_BASE_PATH}"
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Failed while formatting Terraform Code! Please look into your code"
+    sendFailNotification(
+      message: "Failed while linting Terraform Code! Please look into your code"
+    )
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    validateTerraformCode(
+      codeBasePath: "${config.CODE_BASE_PATH}"
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Failed while Terraform Code Validation! Please look into your code"
+    sendFailNotification(
+      message: "Failed while Terraform Code Validation! Please look into your code"
+    )
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    lintingTerraformCode(
+      codeBasePath: "${config.CODE_BASE_PATH}"
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Failed while linting Terraform Code! Please look into your code"
+    sendFailNotification(
+      channelName: "${config.SLACK_CHANNEL_NAME}",
+      message: "Failed while linting Terraform Code! Please look into your code"
+    )
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    planInfrastructure(
+      codeBasePath: "${config.CODE_BASE_PATH}",
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Failed during planning Infrastructure"
+    sendFailNotification(
+      message: "Failed while planning"
+    )
+    echo e.toString()
+    throw e
+  }
+  try 
+  {
+    //input message: 'Press Yes to apply changes', ok: 'YES'
+    createInfrastructure(
+      codeBasePath: "${config.CODE_BASE_PATH}"
+    )
+  } 
+  catch (Exception e) 
+  {
+    echo "Unable to Apply Terraform"
+    sendFailNotification(
+      message: "Failed while applying"
+    )
+    echo e.toString()
+    throw e
+  }
+  sendSuccessNotification(
+    message: "Terraform build <strike>Successfully applied</strike>"   
+  )
 }
