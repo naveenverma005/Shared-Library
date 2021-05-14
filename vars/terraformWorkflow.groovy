2d68625213bf04e7eab1f1653cc84f76484dfa9d
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-05-14 10:35:11 +0530

    Update terraformWorkflow.groovy

--- vars/terraformWorkflow.groovy
+++ vars/terraformWorkflow.groovy
@@ -205,8 +205,3 @@ def call(Map stepParams) {
         message: "Successfully applied"   
     )
 }
-def sendSlackNotification(Map stepParams) {
-    slackSend channel: "${stepParams.slackChannel}",
-    color: "${stepParams.buildStatus}",
-    message: "JOB_NAME:- ${env.JOB_NAME}\n BUILD_URL:- ${env.BUILD_URL}\n"
-}
