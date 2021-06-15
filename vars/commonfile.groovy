27fec328de5bc99b4c40ff1cf202013724c46d95
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-06-15 11:41:45 +0530

    Update commonfile.groovy

--- vars/commonfile.groovy
+++ vars/commonfile.groovy
@@ -4,3 +4,9 @@ def readPropertyFile(Map stepParams) {
     config = readProperties file: "${stepParams.configFilePath}"
     return config
 }
+
+def cleanWorkspace() {
+    stage("Clean WorkSpace") {
+        cleanWs()
+    }
+}
