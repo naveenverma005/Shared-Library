62d735017b194872d27aa725b5224645e0513545
naveenverma005 <82157280+naveenverma005@users.noreply.github.com>
2021-07-23 12:29:49 +0530

    Create laravel&API_test_buildDeployer.groovy

---
+++ vars/laravel&API_test_buildDeployer.groovy
@@ -0,0 +1,136 @@
+#!/usr/bin/env groovy
+def callLaravel (Map stepsParams) 
+{
+    commonfile.cleanWorkspace()
+    try
+    {
+        gitCheckout.checkoutCode()
+    }
+    catch(Exception e)
+    {
+        echo "Unable to clone the Repository"
+        echo e.toString()
+        throw e
+    }
+
+    try 
+    {
+        config = commonfile.readPropertyFile(
+            configFilePath: "${stepsParams.configFilePath}"
+        )
+    }
+    catch (Exception e) 
+    {
+        echo "Sorry I'm unable to read Config file"
+        echo e.toString()
+        throw e
+    }
+
+    stage('Code Build')
+    {
+        try
+        {
+            echo 'Running Code Base'
+            echo 'Updating composer dependencies'
+            sh 'composer update'
+            echo 'Installing dependencies'
+            sh 'composer install'
+        }
+        catch (Exception e)
+        {
+            echo "Failed while Code Build"
+            echo e.toString()
+            throw e
+        }
+    }
+
+    stage('Checking Code Vulnerabilities') 
+    {
+        try
+        {
+            dependencyCheck additionalArguments: '', odcInstallation: 'cfast-OWASP'
+            dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+        }
+        catch(Exception e)
+        {
+            echo "Failed to Code Vulnerabilities"
+            echo e.toString()
+            throw e
+        }
+    }
+
+    stage('Checking Code-Quality')
+    {
+        try{
+            // sh 'sudo rm -rf .scannerwork'
+            // def scannerHome = tool 'SonarQubeScanner';
+            // withSonarQubeEnv('SONAR_HOST_URL')
+            // {
+            //     sh "${scannerHome}/sonar-scanner"
+            // }
+        } 
+        catch(Exception e)
+        {
+            echo "Failed to check code quality "
+            echo e.toString()
+            throw e
+        }
+    }
+    try
+    {
+        gitSecrets.provisionReporting(
+            git_url: "${config.git_url}"
+        )
+    }
+    catch(Exception e)
+    {
+        echo "Unable to Scan Credentials"
+        echo e.toString()
+        throw e
+    }
+    if("${config.application}" == "laravel")
+    {
+        if ( "${Deploy}" == "true" )
+        {
+            stage('Taking Backup')
+            {
+                sh "bash file_comparison.sh ${config.user} ${config.ipaddress} ${config.applicationPath} ${config.applicationBackup}"
+            }
+            stage('Deploying to the Production Environment')
+            {
+                sh " rsync -avz --no-perms --exclude '.git' ${WORKSPACE}/* ${config.user}@${config.ipaddress}:${config.applicationPath}"
+            }
+        }
+
+        if ( "${Rollback}" == "true" )
+        {
+            stage('Rollingback to the previous version')
+            {
+                sh " ssh -i /var/lib/jenkins/testServer.pem ${config.user}@${config.ipaddress} 'cp -rf ${config.applicationBackup}* ${config.applicationPath} '"
+            }
+        }
+    }
+
+    if("${config.application}" == "API")
+    {
+        if ( "${Deploy}" == "true" )
+        {
+            stage('Taking Backup')
+            {
+                sh "bash file_comparison.sh ${config.user} ${config.ipaddress} ${config.applicationPath} ${config.applicationBackup}"
+            }
+            stage('Deploying to the Test Environment')
+            {
+                sh " rsync -avz --no-perms --exclude '.git' ${WORKSPACE}/* ${config.user}@${config.ipaddress}:${config.applicationPath}"
+            }
+        }
+
+        if ( "${Rollback}" == "true" )
+        {
+            stage('Rollingback to the previous version')
+            {
+                sh " ssh -i /var/lib/jenkins/testServer.pem ${config.user}@${config.ipaddress} 'cp -rf ${config.applicationBackup}* ${config.applicationPath} '"
+            }
+        }
+    }
+}
